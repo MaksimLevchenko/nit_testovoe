a45af6804da3dea25718ef332dc97a9aeb610029
EliteNoobest <Valvekat@mail.ru>
2024-07-02 21:31:54 +0300

    Added early bottom sheet

--- lib/main_screen/widgets/home_screen_button_widget.dart
+++ lib/main_screen/widgets/home_screen_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreenButtonWidget extends StatelessWidget {
   const HomeScreenButtonWidget({
@@ -13,14 +14,34 @@ class HomeScreenButtonWidget extends StatelessWidget {
     );
   }
 
-  _showBottomSheet(BuildContext context) {
-    Scaffold.of(context).showBottomSheet((_) {
-      return BottomSheet(
+  void _showBottomSheet(BuildContext context) {
+    showBottomSheet(
+      context: context,
+      builder: (_) {
+        return BottomSheet(
+          clipBehavior: Clip.antiAlias,
+          enableDrag: false,
           onClosing: _onClosing,
-          builder: (_) {
-            return const Text('lol');
-          });
-    });
+          builder: _bottomSheetBuilder,
+        );
+      },
+    );
+  }
+
+  Widget _bottomSheetBuilder(BuildContext _) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: Container(
+            height: 100,
+            color: Colors.red,
+            child: Center(child: Text(index.toString())),
+          ),
+        );
+      },
+      itemCount: 5,
+    );
   }
 
   Widget? _onClosing() {
